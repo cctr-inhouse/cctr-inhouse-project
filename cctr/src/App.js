67472e4d532ad17c6abfb14ab25c716a91ba837d
SATYAMgupta18 <so9121@srmist.edu.in>
2024-05-05 13:32:27 +0530

    login/signup

--- cctr/src/App.js
+++ cctr/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './components/home/home';
+import Login from './login-signup/login/Login';
+import Signup from './login-signup/signup/Signup';
 const App = () => {
   return (
     <div>
-      
+      <Home/>
     </div>
   )
 }
---
+++ cctr/src/components/home/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from '../../nav/nav'
+function Home() {
+  return (
+    <div>
+        <header>
+            <Nav/>
+        </header>
+        <footer>
+            
+        </footer>
+    </div>
+  )
+}
+
+export default Home
